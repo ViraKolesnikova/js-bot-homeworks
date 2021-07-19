e69ef8329bee77da7b679609b54803ccb8de6b03
ViraKolesnikova <82116461+ViraKolesnikova@users.noreply.github.com>
2021-07-19 17:00:27 +0300

    bot

---
+++ index.js
@@ -0,0 +1,1 @@
+import './js/module-2bot.js';
---
+++ js/module-2bot.js
@@ -0,0 +1,63 @@
+//Task 1
+// function checkAge(age) {
+//   if (age >= 18) { // Change this line
+//     return "You are an adult";
+//   }
+
+//   return "You are a minor";
+// }
+// console.log(checkAge(18))
+
+//Task 2
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = "jqueryismyjam";
+//   // Change code below this line
+
+//   if (password === ADMIN_PASSWORD) {
+//     return "Welcome!";
+//   } 
+  
+//   return "Access denied, wrong password!";
+// }
+// console.log(checkPassword("jquesmyjam"))
+// Change code above this line
+
+
+//Task 3
+// function checkStorage(available, ordered) {
+//   // Change code below this line
+  
+//   if (ordered === 0) {
+//     return "Your order is empty!";
+//   } 
+//   if (ordered > available) {
+//     return "Your order is too large, not enough goods in stock!";
+//   } 
+  
+//   return "The order is accepted, our manager will contact you";
+  
+//   // Change code above this line
+// }
+// console.log(checkStorage(20, 5))
+
+//Task 4
+// Change code below this line
+// const fruits = ["apple", "plum", "pear", "orange"];
+
+//Task 5
+
+//Task 6
+
+//Task 7
+
+//Task 8
+
+//Task 9
+
+//Task 10
+
+//Task 11
+
+//Task 12
+
+//Task 13
