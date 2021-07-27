a4c499c71de9afe60b4c298a5f0c884059ecde80
ViraKolesnikova <82116461+ViraKolesnikova@users.noreply.github.com>
2021-07-27 23:10:51 +0300

    bot

--- js/module-3bot.js
+++ js/module-3bot.js
@@ -60,10 +60,126 @@
 // console.log(product.price);
 
 //TASK 9
+// const emailInputName = 'email';
+// const passwordInputName = 'password';
+
+// const credentials = {
+//   // Change code below this line
+//   [emailInputName]: "henry.carter@aptmail.com",
+//   [passwordInputName]: "jqueryismyjam",
+
+
+//   // Change code above this line
+// };
+
 
 
 //TASK 10
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+// const keys = [];
+// const values = [];
+// // Change code below this line
+// for (const element in apartment) {
+//   // console.log(apartment[element]);
+//   keys.push(element)
+//   values.push(apartment[element])
+// }
+// console.log(keys);
+// console.log(values);
+
 
 //TASK 11
+// const keys = [];
+// const values = [];
+// const advert = {
+//   service: "apt",
+// };
+// const apartment = Object.create(advert);
+// apartment.descr = "Spacious apartment in the city center";
+// apartment.rating = 4;
+// apartment.price = 2153;
+
+// for (const key in apartment) {
+//   // Change code below this line
+//   if (apartment.hasOwnProperty(key)) {
+//     keys.push(key);
+//     values.push(apartment[key]);
+//   }
+
+//   // Change code above this line
+// }
+// console.log(values);
 
 //TASK 12
+// function countProps(object) {
+//   let propCount = 0;
+//   // Change code below this line
+//   for(const element in object) {
+//   if (object.hasOwnProperty(element)) {
+//     propCount +=1;
+//   }
+//  }
+//   // Change code above this line
+//   return propCount;
+// }
+
+//TASK 13
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+// const values = [];
+// // Change code below this line
+// const keys = Object.keys(apartment);
+// // console.log(keys);
+// for(let key of keys) {
+  
+//   values.push(apartment[key])
+// }
+// console.log(values);
+// console.log(keys);
+
+
+//TASK 14
+// function countProps(object) {
+//   // Change code below this line
+//   let propCount = 0;
+
+//   const keys = Object.keys(object)
+//   for (const key of keys) {
+//      propCount += 1;
+    
+//   }
+
+//   return propCount;
+//   // Change code above this line
+// }
+// console.log(countProps({ mail: "poly@mail.com", isOnline: true, score: 500 }));
+
+//TASK 15
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+// // Change code below this line
+// const keys = Object.keys(apartment);
+// const values = Object.values(apartment);
+// console.log(values);
+
+//TASK 16
+
+//TASK 17
+
+//TASK 18
+
+//TASK 19
+
+//TASK 20
+
+//TASK 21
