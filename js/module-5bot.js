e088fc0ddb9089ebed025362070ec4fbe935d5a8
ViraKolesnikova <82116461+ViraKolesnikova@users.noreply.github.com>
2021-08-09 22:03:29 +0300

    bot

--- js/module-5bot.js
+++ js/module-5bot.js
@@ -61,7 +61,108 @@
 
 
 //TASK-5
+// function Car({ brand, model, price }) {
+//   this.brand = brand;
+//   this.model = model;
+//   this.price = price;
+// }
+
+// Car.prototype.getPrice = function () {
+//   return this.price;
+// }
+
+// Car.prototype.changePrice = function (newPrice) {
+//   this.price = newPrice;
+//   return newPrice
+// }
+
+// const myCar = new Car({ brand: 'Audi', model: 'Q3', price: 36000 })
+
+// console.log(myCar.changePrice(55000))
+
 
 //TASK-6
+// function Storage (items = []) {
+//   this.items = items;
+// }
+
+// Storage.prototype.getItems = function () {
+//   return this.items
+// }
+
+// Storage.prototype.addItem = function (newItem) {
+//   return this.items.push(newItem)
+// }
+
+// Storage.prototype.removeItem = function (item) {
+//   for (let i = 0; i < this.items.length; i+=1) {
+//     this.items[i] === item ? this.items.splice(i, 1) : false;    
+//   }  
+// }
+
+// const storage = new Storage(['Нанитоиды', 'Пролонгер', 'Антигравитатор']);
+// console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор"]
+// storage.addItem('Дроид');
+// console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор", "Дроид"]
+// storage.removeItem('Пролонгер');
+// console.log(storage.getItems()); // ["Нанитоиды", "Антигравитатор", "Дроид"]
 
 //TASK-7
+// function StringBuilder(baseValue) {
+//   this.value = baseValue;
+// }
+
+// StringBuilder.prototype.getValue = function () {
+//   return this.value;
+// }
+
+// StringBuilder.prototype.padStart = function (str) {
+//   this.value = str + this.value;
+// }
+
+// StringBuilder.prototype.padEnd = function (str) {
+//   this.value = this.value + str;
+// }
+
+// StringBuilder.prototype.padBoth = function (str) {
+//   this.value = str + this.value + str;
+// }
+
+// // Пиши код выше этой строки
+// const builder = new StringBuilder('.');
+// console.log(builder.getValue()); // '.'
+// builder.padStart('^');
+// console.log(builder.getValue()); // '^.'
+// builder.padEnd('^');
+// console.log(builder.getValue()); // '^.^'
+// builder.padBoth('=');
+// console.log(builder.getValue()); // '=^.^='
+
+//TASK-8
+//об’явлення через клас
+// class Car {
+
+// }
+// const myCar = new Car;
+// console.log(myCar);
+
+//TASK-9
+//Об’явлення через клас та конструктор
+// class Car {
+//   constructor({ brand, model, price }) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+// }
+  
+
+//TASK-10
+
+//TASK-11
+
+//TASK-12
+
+//TASK-13
+
+//TASK-14
